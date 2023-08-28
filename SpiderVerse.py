7eb4adf0ad035d3259cd3afaf3383f786adda912
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2023-08-28 10:46:45 +0530

    Changes to be committed: 	modified:   SpiderVerse.py 	modified:   Spiderman.py

--- SpiderVerse.py
+++ SpiderVerse.py
@@ -4,7 +4,7 @@ df = pd.read_csv("SpiderVerse.csv")
 Stat = [df['Rating']]
 
 # print(df.describe())
-print(df['Rating'].quantile(0.5))
+print("The Median is",df['Rating'].quantile(0.5))
 
 a = (df['Rating'].quantile(0.25))
 b = (df['Rating'].quantile(0.75))
