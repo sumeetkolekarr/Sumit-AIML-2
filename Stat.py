521c574dcdf3b70714f1a3c871f558feecedd807
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2023-08-29 13:31:05 +0530

    Changes to be committed: 	modified:   Stat.py

--- Stat.py
+++ Stat.py
@@ -1,8 +1,10 @@
 import pandas as pd
 df = pd.read_csv("imdb-spider-man-reviews.csv")
 
+# Separating Ratings According to the movie 
 Anim = df.loc[df['Movie'] == 'Spider-Man: Into the Spider-Verse']
 Real = df.loc[df['Movie'] == 'Spider-Man']
 
+# Adding Files to the given Sheets
 Anim.to_csv("SpiderVerse.csv")
 Real.to_csv("SpiderMan.csv")
